6ae069d5c6d0b9a2659e005a712860de7704b9e4
Yogesh Vashisth <writetokhair@gmail.com>
2024-06-17 20:58:49 +0530

    user data added

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ import { User } from "./models/user.js";
 
 dotenv.config();
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(bodyParser.json());
+
 // Routes
 app.use("/api/v1/users", userRoutes);
 
